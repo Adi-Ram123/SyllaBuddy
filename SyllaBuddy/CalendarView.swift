bfe99f0233362a2810fc3fd919c32246e1409f82
Adi-Ram123 <adi.sr@outlook.com>
2025-07-10 17:21:41 -0500

    Started authentication

---
+++ SyllaBuddy/CalendarView.swift
@@ -0,0 +1,30 @@
+//
+//  CalendarView.swift
+//  SyllaBuddy
+//
+//  Created by Aditya Ramaswamy on 7/10/25.
+//
+
+import UIKit
+
+class CalendarView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //self.navigationItem.setHidesBackButton(true, animated: false) //Hide and disable nav bar
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
